16056665889ad98bf77213d6c92011740388b133
rbrissonneau2018 <rbrissonneau2018@22-UC34-20.ad.campus-eni.fr>
2018-11-30 11:48:43 +0100

    nouvelle variable vitesseMax g/setter

--- Course/src/fr/eni/formation/Bo/Adversaire.java
+++ Course/src/fr/eni/formation/Bo/Adversaire.java
@@ -1,5 +1,15 @@
 package fr.eni.formation.Bo;
 
 public class Adversaire {
+	private Integer vitesseMax;
+	
 
+	public Integer getVitesseMax() {
+		return vitesseMax;
+	}
+
+	public void setVitesseMax(Integer vitesseMax) {
+		this.vitesseMax = vitesseMax;
+	}
+	
 }
