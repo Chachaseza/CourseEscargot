3945a197f59ebad0e2e39e8c6c36453c64b5fd45
rbrissonneau2018 <rbrissonneau2018@22-UC34-20.ad.campus-eni.fr>
2018-11-30 10:52:08 +0100

    Add nbParticipants, longeur,constructeur get/setter  et la piste

--- Course/src/fr/eni/formation/Bo/Piste.java
+++ Course/src/fr/eni/formation/Bo/Piste.java
@@ -1,5 +1,37 @@
 package fr.eni.formation.Bo;
 
 public class Piste {
-
+	private Integer nbParticipants;
+	private Integer longeur;
+	private String [][]piste;
+	
+	
+	
+	public Piste(Integer nbParticipants, Integer longeur) {
+		super();
+		this.nbParticipants = nbParticipants;
+		this.longeur = longeur;
+		setPiste();
+	}
+	public Integer getNbParticipants() {
+		return nbParticipants;
+	}
+	public void setNbParticipants(Integer nbParticipants) {
+		this.nbParticipants = nbParticipants;
+	}
+	public Integer getLongeur() {
+		return longeur;
+	}
+	public void setLongeur(Integer longeur) {
+		this.longeur = longeur;
+	}
+	public String[][] getPiste() {
+		return piste;
+	}
+	public void setPiste() {
+		if(piste==null){
+		this.piste = new String[getNbParticipants()][getLongeur()];
+		}
+	}
+	
 }
